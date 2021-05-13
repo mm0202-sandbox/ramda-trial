86fbf3e1369fc2f30a405ac823a74ac8d9c894cd
mm0202 <for_etc@hotmail.com>
2021-05-13 13:38:04 +0900

    SB-1834 不要ファイル、不要部分を削除

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,6 @@
-// noinspection HtmlUnknownTag
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['.bk', 'dist'],
-  moduleNameMapper: {
-    '^~/(.*)$': '<rootDir>/$1',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
-  },
-  setupFiles: ['./jest.setup.ts'],
   reporters: ['default', ['jest-junit', { outputDirectory: 'coverage' }]],
   coverageReporters: ['text', 'cobertura'],
-  // globals: {
-  //     'ts-jest': {
-  //         tsConfig: 'jest.tsconfig.json',
-  //     },
-  // },
 }
